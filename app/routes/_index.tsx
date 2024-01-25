81bc940b536cb9a99e2591f92f6f440ea7c92b64
berlin <1580940252@qq.com>
2024-01-25 18:29:23 +0800

    feat: update

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
+import { defer, type MetaFunction } from "@remix-run/node";
+import { Await, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,9 +9,43 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+   // 👇 note this promise is not awaited
+   const reviewsPromise = new Promise<number>((r => {
+    setTimeout(() => {
+      // fetch('http://localhost:8080/123')
+      r(100);
+    }, 5000)
+   }))
+   // 👇 but this one is
+   const product = await new Promise<number>((r => {
+    setTimeout(() => {
+      // fetch('http://localhost:8080/1234')
+      r(101);
+    }, 2000)
+   }))
+
+   console.log('loader',loader)
+ 
+   return defer({
+     product,
+     reviews: reviewsPromise,
+   });
+}
+
 export default function Index() {
+  const { product, reviews } =
+  useLoaderData<typeof loader>();
+
+  console.log('product',product,reviews)
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <div>{product}</div>
+      <Suspense fallback={<div>12314</div>}>
+        <Await resolve={reviews}>
+          {(reviews) => <div>{reviews}</div>}
+        </Await>
+      </Suspense>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
